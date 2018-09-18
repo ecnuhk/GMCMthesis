87435cd50ca8da7f9d47a4a6bd24231f34488f0c
xiaomogd@gmail.com <K710furww>
2018-09-19 00:45:36 +0800

    00:45

--- Q1.py
+++ Q1.py
@@ -1,6 +1,9 @@
+import copy
+
 from scipy import optimize, math
 from numpy.linalg import norm
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Answer:
@@ -90,15 +93,9 @@ class Answer:
                 x0 = X[Hk][Ri][Pj]['x']
                 y0 = X[Hk][Ri][Pj]['y']
 
-                # 一条飞行方案
-                rst = {
-                    'Ri': Ri,
-                    'Pj': Pj,
-                    'Hk': Hk,
-                    'X': [{'x': x0, 'y': y0, 't': Pj}],  # 方案路过的所有点
-                    'para': None  # 方案的直线参数
-                }
                 for dirc in range(Pj + 1, 20):  # 尝试从Pj出发的所有方向
+                    line_x, line_y, line_t = [x0], [y0], [Pj]
+
                     # 确定方向
                     x1 = X[Hk][Ri][dirc]['x']
                     y1 = X[Hk][Ri][dirc]['y']
@@ -107,10 +104,20 @@ class Answer:
                     if not self.is_dirc_ok(x0, y0, x1, y1, (dirc - Pj) * 10):
                         continue
 
+                    # 一条飞行方案
+                    rst = {
+                        'Ri': Ri,
+                        'Pj': Pj,
+                        'Hk': Hk,
+                        'X': [],  # 方案路过的所有点
+                        'X_idx': [(Ri, Pj, Hk), (Ri, dirc, Hk)],
+                        'para': None  # 方案的直线参数
+                    }
+
                     # 加入方向点
-                    line_x = [x0, x1]
-                    line_y = [y0, y1]
-                    line_t = [Pj, dirc]  # 时间序列点
+                    line_x.append(x1)
+                    line_y.append(y1)
+                    line_t.append(dirc)  # 时间序列点
 
                     # 直线拟合与绘制
                     A, B = optimize.curve_fit(lambda x, A, B: A * x + B, line_x, line_y)[0]
@@ -133,12 +140,12 @@ class Answer:
 
                     # 生成方案
                     rst['para'] = {'A': A, 'B': B}
+
                     for i in range(len(line_t)):
-                        if i == 0:
-                            continue
                         rst['X'].append({'x': line_x[i], 'y': line_y[i], 't': line_t[i]})
-                # 加入方案
-                lines.append(rst)
+
+                    # 加入方案
+                    lines.append(rst)
         return lines
 
     # 计算平面2点距
@@ -180,68 +187,268 @@ class Answer:
                     return False
             return True
 
+    # 输出所有航线，并绘制
+    def paint_lines(self):
+        self.get_points_by_all_height()
 
-# 输出所有航线
-def paint_lines():
-    import matplotlib.pyplot as plt
-    answer = Answer()
-    answer.get_points_by_all_height()
+        fig = plt.figure()
 
-    fig = plt.figure()
+        for height in range(2000, 2501, 100):
 
-    for height in range(2000, 2501, 100):
+            ax = fig.add_subplot(2, 3, (height - 2000) / 100 + 1)
+            ax.set_xlabel('X Label', color='r')
+            ax.set_ylabel('Y Label', color='g')
+            for Ri in range(len(self.X[height])):
+                each = self.X[height][Ri]
+                for Pj in range(len(each)):
+                    each2 = each[Pj]
+                    s = ax.scatter(each2['x'], each2['y'], c='r', marker='o', label=height)
+                    # ax.text(each2['x'], each2['y'], "(%d,%d)" % (each2['x'], each2['y']))
+                    ax.text(each2['x'], each2['y'], "(%d,%d)" % (Ri, Pj))
 
-        ax = fig.add_subplot(2, 3, (height - 2000) / 100 + 1)
-        ax.set_xlabel('X Label', color='r')
-        ax.set_ylabel('Y Label', color='g')
-        for Ri in range(len(answer.X[height])):
-            each = answer.X[height][Ri]
-            for Pj in range(len(each)):
-                each2 = each[Pj]
-                s = ax.scatter(each2['x'], each2['y'], c='r', marker='o', label=height)
-                # ax.text(each2['x'], each2['y'], "(%d,%d)" % (each2['x'], each2['y']))
-                ax.text(each2['x'], each2['y'], "(%d,%d)" % (Ri, Pj))
+            ax.legend([s], ['z: ' + str(height)])
+            lines = self.get_legal_flight_by_height(height)
 
-        ax.legend([s], ['z: ' + str(height)])
-        lines = answer.get_legal_flight_by_height(height)
+            for each in lines:
+                print(each['Hk'], each['Ri'], each['Pj'], each['para'], sep='\t', end='\t')
+                if each['para'] is not None:
+                    # A = each['para']['A']
+                    # B = each['para']['B']
+                    # x = [each['X'][0]['x'], each['X'][-1]['x']]
+                    # x = np.array(x)
+                    # y = A * x + B
+                    # ax.plot(x, y)
+                    x = []
+                    y = []
+                    for i in range(len(each['X'])):
+                        x.append(each['X'][i]['x'])
+                        y.append(each['X'][i]['y'])
+                    ax.plot(x, y, linewidth=1, linestyle='dashed', alpha=0.5)
 
-        line_count = 1
-        for each in lines:
-            # print(each['Hk'], each['Ri'], each['Pj'], each['para'], sep='\t', end='\t')
-            if each['para'] is not None:
-                # A = each['para']['A']
-                # B = each['para']['B']
-                # x = [each['X'][0]['x'], each['X'][-1]['x']]
-                # x = np.array(x)
-                # y = A * x + B
-                # ax.plot(x, y)
-                x = []
-                y = []
-                for i in range(len(each['X'])):
-                    x.append(each['X'][i]['x'])
-                    y.append(each['X'][i]['y'])
-                ax.plot(x, y, linewidth=line_count, linestyle='dashed', alpha=0.7)
-                line_count += 0.2
-                # for Xs in each['X']:
-                #     print((Xs['t'], Xs['x'], Xs['y']), end='\t')
-                # print()
+                    for Xs in each['X']:
+                        print((Xs['t'], Xs['x'], Xs['y']), end='\t')
+                    print()
 
-    plt.show()
+        plt.show()
 
+    # 输出所有航线
+    def get_all_plans_and_paint(self):
+        self.get_points_by_all_height()
 
-# 输出全交点坐标
-def get_all_points():
-    answer = Answer()
-    answer.get_points_by_all_height()
-    for height in answer.X.keys():
-        # print(height)
-        for Ri in range(len(answer.X[height])):
-            R = answer.X[height][Ri]
-            for Pj in range(len(R)):
-                P = R[Pj]
-                print(Ri + 1, Pj + 1, P['x'], P['y'], P['z'])
-                # print()
+        plans = []
+        for height in range(2000, 2501, 100):
+
+            lines = self.get_legal_flight_by_height(height)
+
+            for each in lines:
+
+                # print(each['Hk'], each['Ri'], each['Pj'], each['para'], sep='\t', end='\t')
+                if each['para'] is not None:
+                    print(each['X_idx'])
+                    plans.append(each['X_idx'])
+
+        import matplotlib.pyplot as plt
+        fig = plt.figure()
+        for height in range(2000, 2501, 100):
+
+            ax = fig.add_subplot(2, 3, (height - 2000) / 100 + 1)
+            ax.set_xlabel('X Label', color='r')
+            ax.set_ylabel('Y Label', color='g')
+            for Ri in range(len(self.X[height])):
+                each = self.X[height][Ri]
+                for Pj in range(len(each)):
+                    each2 = each[Pj]
+                    s = ax.scatter(each2['x'], each2['y'], c='r', marker='o', label=height)
+                    # ax.text(each2['x'], each2['y'], "(%d,%d)" % (each2['x'], each2['y']))
+                    ax.text(each2['x'], each2['y'], "(%d,%d)" % (Ri, Pj))
+
+            ax.legend([s], ['z: ' + str(height)])
+
+            for each in plans:
+                if each[0][2] == height:
+                    X1 = self.X[height][each[0][0]][each[0][1]]
+                    X2 = self.X[height][each[1][0]][each[1][1]]
+                    x = [X1['x'], X2['x']]
+                    y = [X1['y'], X2['y']]
+                    ax.plot(x, y)
+
+        plt.show()
+
+    # 输出全交点坐标
+    def get_all_points(self):
+        self.get_points_by_all_height()
+        for height in self.X.keys():
+            # print(height)
+            for Ri in range(len(self.X[height])):
+                R = self.X[height][Ri]
+                for Pj in range(len(R)):
+                    P = R[Pj]
+                    print(Ri + 1, Pj + 1, P['x'], P['y'], P['z'])
+                    # print()
+
+    def get_plans(self):
+        self.get_points_by_all_height()
+        plans = []
+        for height in range(2000, 2501, 100):
+            lines = self.get_legal_flight_by_height(height)
+            for each in lines:
+                # print(each['Hk'], each['Ri'], each['Pj'], each['para'], sep='\t', end='\t')
+                if each['para'] is not None:
+                    # print(each['X_idx'])
+                    plans.append(each['X_idx'])
+
+        return plans
+
+    def result(self):
+        plans = self.get_plans()
+        self.plan_by_Pj = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+        for each in plans:
+            start, end = each[0], each[1]
+            row = {
+                'start': start,
+                'end': end,
+                'choose': False,
+                'from': None
+            }
+            self.plan_by_Pj[start[1]].append(row)
+            self.plan_by_Pj[end[1]].append(row)
+
+        # for each in self.plan_by_Pj:
+        #     print(each)
+
+        # 寻解
+        self.stack = []
+        self.choice = [len(self.plan_by_Pj[i]) for i in range(20)]  # 每个虚假点的可选航迹数目
+        self.satisfy = [0 for i in range(20)]  # 已选航迹数目
+        self.answer = {
+            'number': 999999,
+            'answer': [None for i in range(999)]
+        }
+        need_pop = False
+        while not self.is_satisfy():
+            if need_pop:
+                row = self.get_next_line()
+                need_pop = False
+            else:
+                min_Pj = self.get_min_choice_Pj()  # 找到可选航迹数最少的虚假点
+                row = self.get_line_from_Pj(min_Pj)  # 取得航迹
+            if row is None:
+                # 记录此状态需要无人机数量
+                number = self.get_un_satisfy_num() + len(self.stack)
+
+                # 优先选数量少的
+                if number < self.answer['number']:
+                    self.answer['number'] = number
+                    self.answer['answer'] = self.stack[:]
+
+                    # 打印一下
+                    print('需要%d架无人机' % number, end='\t')
+                    self.print_stack()
+                else:
+                    # 回退
+                    need_pop = True
+            else:
+                self.stack.append(row)
+        print(self.stack)
+
+    def is_satisfy(self):
+        for each in self.satisfy:
+            if each < 3:
+                return False
+        return True
+
+    def get_min_choice_Pj(self):
+        min_Pj = None
+
+        # 找到第一个正数Pj
+        for i in range(20):
+            if self.choice[i] > 0:
+                min_Pj = i
+
+        # 没有可选项了
+        if min_Pj is None:
+            return None
+
+        for i in range(20):
+            if self.choice[i] < self.choice[min_Pj]:
+                min_Pj = i
+
+        return min_Pj
+
+    def get_line_from_Pj(self, Pj):
+        if Pj is None:
+            return None
+
+        for row in self.plan_by_Pj[Pj]:
+            if row['choose']:
+                continue
+            else:
+                row['choose'] = True
+                row['from'] = Pj
+                # 更新选择数
+                start_Pj = row['start'][1]
+                end_Pj = row['end'][1]
+                # 更新选择数
+                self.choice[start_Pj] -= 1
+                self.choice[end_Pj] -= 1
+                # 更新已满足虚假点数
+                self.satisfy[start_Pj] += 1
+                self.satisfy[end_Pj] += 1
+                return row
+        return None
+
+    def get_un_satisfy_num(self):
+        count = 0
+        for each in self.satisfy:
+            if each < 3:
+                count += 3 - each
+        return count
+
+    def get_next_line(self):
+        # 找到来源
+        pre = self.stack.pop()
+        start_Pj = pre['start'][1]
+        end_Pj = pre['end'][1]
+
+        # 清除访问
+        pre['choose'] = False
+        self.choice[start_Pj] += 1
+        self.choice[end_Pj] += 1
+        self.satisfy[start_Pj] -= 1
+        self.satisfy[end_Pj] -= 1
+
+        Pj = pre['from']  # 上一步的欲选择虚假点
+        pre['from'] = None
+
+        find_pre = False
+        for row in self.plan_by_Pj[Pj]:
+            if not find_pre:
+                if row == pre:
+                    find_pre = True
+            else:
+                row['choose'] = True
+                row['from'] = Pj
+                # 更新选择数
+                start_Pj = row['start'][1]
+                end_Pj = row['end'][1]
+                # 更新选择数
+                self.choice[start_Pj] -= 1
+                self.choice[end_Pj] -= 1
+                # 更新已满足虚假点数
+                self.satisfy[start_Pj] += 1
+                self.satisfy[end_Pj] += 1
+                return row
+
+        return None
+
+    def print_stack(self):
+        for each in self.stack:
+            start = each['start']
+            end = each['end']
+            print('(%d,%d,%d)->(%d,%d,%d)' % (start[0], start[1], start[2], end[0], end[1], end[2]), end='\t')
+        print()
 
 
 if __name__ == '__main__':
-    paint_lines()
+    ans = Answer()
+    ans.result()
--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-from scipy import optimize, math
-from numpy.linalg import norm
-import numpy as np
-import matplotlib.pyplot as plt
-
-line_x = [75522.07977, 75562.18182, 75585.56548, 75591.91457, 75579.54545, 75547.2155, 75493.33333]
-line_y = [23752.49288, 24095.63636, 24485.49107, 24927.91762, 25429.85893, 26000.40355, 26648.75]
-A, B = optimize.curve_fit(lambda x, A, B: A * x + B, line_x, line_y)[0]
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(line_x, line_y)
-ax.plot([75600,line_x[-1]], [A * 75600 + B, A*line_x[-1] + B], c='red')
-plt.show()
+a = []
+b = []
+ob = {'e':123}
+a.append(ob)
+b.append(ob)
+print(a[0] == b[0])
